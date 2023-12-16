b9b3adabd7ae724e4ced18aa607e693fdad044f5
RalphAdi <153388309+RalphAdi@users.noreply.github.com>
2023-12-16 13:04:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,35 +2,35 @@ var box;
 
 function setup() {
   createCanvas(400,400);
-  background(51);
+  backround(51);
   box = createSprite(200,200,30,30);
 
 }
 
-function draw() 
+function draw()
 {
   
 
-  if (keyIsDown(RIGHT_ARROW)) 
+  if(keyIsDown(RIGHT_ARROW))
   {
-    background("red");
+    backround("red");
   }
 
-    if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
+    if(keyIsDown(LEFT_ARROW))
+    {
+    backround("blue");
     
-  }
+    }
  
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
+    if(keyIsDown(UP_ARROW)) 
+    {
+    backround("yellow");
    
-  }
+    }
 
-  if (keyIsDown(DOWN_ARROW)) 
+  if (keyIsDown(DOWN_ARROW))
   {
-    background("green");
+   backround("green")
   }
 
   drawSprites();
